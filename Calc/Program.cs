40071d1e32179dcea4f4e218458c4b4f65e3bdc6
Klaudia <Klaudia@DESKTOP-2GEQ117>
2020-01-20 22:44:28 +0100

    1 commit

---
+++ Calc/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Calc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 100;
+            int b = 50;
+            int c = a + b;
+
+            Console.WriteLine(c);
+            Console.ReadKey();
+
+        }
+    }
+}
