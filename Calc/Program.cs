3feb1e3ada7ff25c67949eae26bb6633d2d35bdd
Klaudia <Klaudia@DESKTOP-2GEQ117>
2020-01-21 20:46:47 +0100

    add main method

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ProgramTexts;
 
 namespace Calc
 {
@@ -6,13 +7,43 @@ namespace Calc
     {
         static void Main(string[] args)
         {
-            int a = 100;
-            int b = 50;
-            int c = a + b;
+            double num1 = 0; double num2 = 0;
 
-            Console.WriteLine(c);
+            Console.WriteLine(ProgramText.Title);
+            Console.WriteLine();
+            Console.WriteLine(ProgramText.SelectNum1);
+            num1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(ProgramText.SelectNum2);
+            num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(ProgramText.SelectMathAction);
+            Console.WriteLine(ProgramText.Add);
+            Console.WriteLine(ProgramText.Subtract);
+            Console.WriteLine(ProgramText.Muliply);
+            Console.WriteLine(ProgramText.Devide);
+            Console.Write(ProgramText.SelectAction);
+            switch (Console.ReadLine())
+            {
+                case "+":
+                    Console.WriteLine($"Your result: {num1} + {num2} = " + (num1 + num2));
+                    break;
+                case "-":
+                    Console.WriteLine($"Your result: {num1} - {num2} = " + (num1 - num2));
+                    break;
+                case "*":
+                    Console.WriteLine($"Your result: {num1} * {num2} = " + (num1 * num2));
+                    break;
+                case "/":
+                    while (num2 == 0)
+                    {
+                        Console.WriteLine(ProgramText.NonZero);
+                        num2 = Convert.ToInt32(Console.ReadLine());
+                    }
+                    Console.WriteLine($"Your result: {num1} / {num2} = " + (num1 / num2));
+                    break;
+            }
+
+            Console.WriteLine(ProgramText.CloseApp);
             Console.ReadKey();
-
         }
     }
 }
---
+++ Calc/ProgramTexts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ProgramTexts
+{
+    public class ProgramText
+    {
+        public static string Title => "Simple Calculator App\r";
+        public static string SelectNum1 => "Select 1st number\r";
+        public static string SelectNum2 => "Select 2nd number\r";
+        public static string SelectMathAction => "Select math operation\r";
+        public static string SelectAction => "Your option? ";
+        public static string Add => "\t+ - Add";
+        public static string Subtract => "\t- - Subtract";
+        public static string Muliply => "\t* - Multiply";
+        public static string Devide => "\t/ - Divide";
+        public static string CloseApp => "Press any key to close the Calculator console app...";
+        public static string NonZero => "Enter a non-zero divisor: ";
+        public static string ResultText => "Your result: ";
+    }
+}
