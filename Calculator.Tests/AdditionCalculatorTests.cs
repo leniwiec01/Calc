2a013ed7ebccb58cb8b0dec734fc097f268e9eb8
Marek Popela <popela.marek@gmail.com>
2020-04-14 19:17:09 +0200

    Add substraction unit tests

---
+++ Calculator.Tests/AdditionCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+
+namespace Calculator.UnitTests
+{
+    [TestClass]
+    public class AdditionCalculatorTests
+    {
+        [TestMethod]
+        public void CheckAdditionOperationPositive()
+        {
+            var logicMethod = new Calculator.CalculatorLogic();
+            var addAction = logicMethod.DoOperation(2, 2, "+");
+            Assert.AreEqual(4, addAction);
+        }
+        [TestMethod]
+        public void CheckAdditionOperationNegative()
+        {
+            var logicMethod = new Calculator.CalculatorLogic();
+            var addAction = logicMethod.DoOperation(2, 2, "+");
+            Assert.AreNotEqual(5, addAction);
+        }
+        [TestMethod]
+        public void CheckAdditionOperationMinusNumbersPositive()
+        {
+            var logicMethod = new Calculator.CalculatorLogic();
+            var addAction = logicMethod.DoOperation(-2, -2, "+");
+            Assert.AreEqual(-4, addAction);
+        }
+    }
+}
---
+++ Calculator.Tests/SubtractionCalculatorTests.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+
+namespace Calculator.UnitTests
+{
+    [TestClass]
+    public class SubtractionCalculatorTests
+    {
+        [TestMethod]
+        public void CheckSubtractionOperationPositive()
+        {
+            var logicMethod = new Calculator.CalculatorLogic();
+            var subtractionAction = logicMethod.DoOperation(2, 2, "-");
+            Assert.AreEqual(0, subtractionAction);
+        }
+        [TestMethod]
+        public void CheckSubtractionOperationNegative()
+        {
+            var logicMethod = new Calculator.CalculatorLogic();
+            var subtractionAction = logicMethod.DoOperation(5, 2, "-");
+            Assert.AreNotEqual(5, subtractionAction);
+        }
+        [TestMethod]
+        public void CheckSubtractionOperationMinusNumbersPositive()
+        {
+            var logicMethod = new Calculator.CalculatorLogic();
+            var subtractionAction = logicMethod.DoOperation(-2, -2, "-");
+            Assert.AreEqual(0, subtractionAction);
+        }
+    }
+}
